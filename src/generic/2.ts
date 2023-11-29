d4e958792980d91dcad71a099e7da95f9406d3c0
AndriySachyk <asachyk1312@gmail.com>
2023-11-29 16:57:42 +0200

    Done homework

--- src/generic/2.ts
+++ src/generic/2.ts
@@ -11,7 +11,7 @@ type AllType = {
   weight: number
 }
 
-function compare (top, bottom): AllType {
+function compare (top:Pick<AllType, 'name'|'color'>, bottom:Pick<AllType, 'position'|'weight'>): AllType {
   return {
     name: top.name,
     color: top.color,
@@ -19,5 +19,8 @@ function compare (top, bottom): AllType {
     weight: bottom.weight,
   }
 }
+
+
+
 
 export {};
